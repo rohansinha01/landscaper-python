d9d67d862c564e316fdfa4b0ef38c33634bd8303
rohansinha01 <rohansinha01@gmail.com>
2024-03-09 22:07:33 -0500

    not able to buy things that don't exist

--- landscaper.py
+++ landscaper.py
@@ -1,15 +1,19 @@
 # Character Attributes
 mower = {
     "money": 0,
-    "tool": 0
+    "tool": 0,
+    "winner": False
 }
 
 
 
 # Tools Available
 tools = [
-    {"name": "teeth", "price": 0,"profit": 5},
-    {"name": "scissors", "price": 5,"profit": 5}
+    {"name": "teeth", "price": 0,"profit": 1},
+    {"name": "scissors", "price": 5,"profit": 5},
+    {"name": "push lawnmower", "price": 25,"profit": 50},
+    {"name": "battery powered lawnmower", "price": 250,"profit": 100},
+    {"name": "team of starving students", "price": 500,"profit": 250},
 ]
 
 # Functions
@@ -36,17 +40,25 @@ def actions():
 def mow():
     print("you mowed")
     # print(tools[1]["profit"])
-    mower["money"] += tools[0]["profit"]
+    mower["money"] += tools[mower["tool"]]["profit"]
     print(f"You have ${mower["money"]}")
     win()
     
 def upgrade():
-    print("you upgraded")
-    if mower["money"] >= tools[0]["price"]:
+    if mower["tool"] + 1 < len(tools) and  mower["money"] >= tools[mower["tool"]]["price"]:
         mower["tool"] += 1
+        mower["money"] -= tools[mower["tool"]]["price"]
         print(f"You have upgraded to {tools[mower["tool"]]["name"]}")
-    # elif mower["money"] <= tools[]
-        # print(tools[mower["tool"]]["name"])    
+        print(f"You have ${mower["money"]}")
+        
+    elif tools[mower["tool"]] == len(tools):
+        print("You have everything!")
+    elif mower["money"] < tools[mower["tool"]]["price"]:
+        print('You do not have the funds')   
+    else: print("No more items to buy!")
+        
+        
+    
     win()
 
 def quit():
@@ -54,6 +66,10 @@ def quit():
 
 def win():
     actions()
+    if mower["money"] >= 1000 and tools[4]["name"] == "team of starving students":
+        print("you have a team and $1000")
+        mower["winner"] == True
 
 actions()
+
 
